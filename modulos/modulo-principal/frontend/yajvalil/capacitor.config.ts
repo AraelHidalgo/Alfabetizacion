3a08b958135994a4db1f3ea734906cf7c7bf85d9
AraelHidalgo <arael.hidalgo54@unach.mx>
2025-03-02 18:23:48 -0600

    Ionic-vue configuration

---
+++ modulos/modulo-principal/frontend/yajvalil/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'yajvalil',
+  webDir: 'dist'
+};
+
+export default config;
